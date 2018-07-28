08e4bbdc7141996193f00d41e511aef45f392e97
SilasReinagel <silas.reinagel@gmail.com>
2018-07-28 00:36:45 -0700

    Displays damage numbers after resolving shots. Closes #188

---
+++ src/ZeroFootprintSociety/CoreGame/ActionEvents/ShotHit.cs
@@ -0,0 +1,11 @@
+﻿using ZeroFootPrintSociety.Characters;
+
+namespace ZeroFootPrintSociety.CoreGame.ActionEvents
+{
+    class ShotHit
+    {
+        public Character Target { get; set; }
+        public Character Attacker { get; set; }
+        public int DamageAmount { get; set; }
+    }
+}
--- src/ZeroFootprintSociety/CoreGame/Mechanics/Resolution/RangedAction.cs
+++ src/ZeroFootprintSociety/CoreGame/Mechanics/Resolution/RangedAction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MonoDragons.Core.EventSystem;
+using ZeroFootPrintSociety.CoreGame.ActionEvents;
 using ZeroFootPrintSociety.CoreGame.StateEvents;
 
 namespace ZeroFootPrintSociety.CoreGame.Mechanics.CombatResolution
@@ -22,7 +23,13 @@ namespace ZeroFootPrintSociety.CoreGame.Mechanics.CombatResolution
             var attackerHitChance = e.Attacker.Stats.AccuracyPercent + attackerWeapon.AccuracyPercent - e.Defender.Stats.Agility;
             for (var i = 0; i < attackerWeapon.NumShotsPerAttack; i++)
                 if (_random.Next(0, 100) < attackerHitChance)
-                    e.Defender.State.RemainingHealth -= (int)(attackerWeapon.DamagePerHit * attackerWeapon.EffectiveRanges[distance]);
+                {
+                    var amt = (int)(attackerWeapon.DamagePerHit * attackerWeapon.EffectiveRanges[distance]);
+                    e.Defender.State.RemainingHealth -= amt;
+                    Event.Publish(new ShotHit { Attacker = e.Attacker, Target = e.Defender, DamageAmount = amt });
+                }
+                else
+                    Event.Publish(new ShotMissed { Attacker = e.Attacker, Target = e.Defender });
 
             if (e.Defender.Gear.EquippedWeapon.IsRanged && e.Defender.State.RemainingHealth > 0)
             {
@@ -30,10 +37,14 @@ namespace ZeroFootPrintSociety.CoreGame.Mechanics.CombatResolution
                 var defenderHitChance = e.Defender.Stats.AccuracyPercent + defenderWeapon.AccuracyPercent - e.Attacker.Stats.Agility;
                     for (var i = 0; i < defenderWeapon.NumShotsPerAttack; i++)
                         if (_random.Next(0, 100) < defenderHitChance)
-                            e.Attacker.State.RemainingHealth -= (int)(defenderWeapon.DamagePerHit * defenderWeapon.EffectiveRanges[distance]);
+                        {
+                            var amt = (int)(defenderWeapon.DamagePerHit * defenderWeapon.EffectiveRanges[distance]);
+                            e.Attacker.State.RemainingHealth -= amt;
+                            Event.Publish(new ShotHit { Attacker = e.Defender, Target = e.Attacker, DamageAmount = amt });
+                        }
+                        else
+                            Event.Publish(new ShotMissed { Attacker = e.Defender, Target = e.Attacker });
             }
-
-            Event.Publish(new ActionResolved());
         }
     }
 }
