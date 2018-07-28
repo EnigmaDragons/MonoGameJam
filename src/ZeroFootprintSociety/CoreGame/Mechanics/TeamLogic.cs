fd1dd93ad6a88da1f4885c9f988b5a54a14626bc
Félix Dion Robidoux <felix@fdrobidoux.com>
2018-07-28 16:18:53 -0400

    Renamed some methods and added comments.

--- src/ZeroFootprintSociety/CoreGame/Mechanics/TeamLogic.cs
+++ src/ZeroFootprintSociety/CoreGame/Mechanics/TeamLogic.cs
@@ -14,18 +14,20 @@ namespace ZeroFootPrintSociety.CoreGame
         
         #region Team Enum Extensions
 
-        public static bool CanShootAt(this Team team, Team teamToShoot)
+        public static bool AllowedToShootAt(this Team team, Team teamToShoot)
         {
             throw new NotImplementedException();
         }
 
-        public static bool CanShootAt(this Team team, TeamGroup teamGroupToShoot)
+        public static bool AllowedToShootAt(this Team team, TeamGroup teamGroupToShoot)
         {
             throw new NotImplementedException();
         }
 
-        public static bool IsPartOfTeamGroup(this Team subjectTeam, TeamGroup teamGroup)
+        public static bool IsIncludedIn(this Team subjectTeam, TeamGroup teamGroup)
         {
+            // Test the `None` group, which always includes nobody.
+            // TODO: See if it's necessary (we might not need to check for `TeamGroup.None` ever).
             if (teamGroup == TeamGroup.None)
                 return false;
 
@@ -33,6 +35,7 @@ namespace ZeroFootPrintSociety.CoreGame
             if (teamGroup == TeamGroup.All || (int)subjectTeam == (int)teamGroup)
                 return true;
 
+            // Test multi-team TeamGroups.
             switch (teamGroup)
             {
                 case TeamGroup.FriendliesAndNeutrals:
@@ -42,9 +45,9 @@ namespace ZeroFootPrintSociety.CoreGame
                 case TeamGroup.NeutralsAndEnemies:
                     return subjectTeam.Equals(Team.Neutral) || subjectTeam.Equals(Team.Enemy);
                 default:
-                    throw new Exception($"Given Multi-Teamed TeamGroup "
+                    throw new Exception($"Given multi-team TeamGroup \""
                                         + Enum.GetName(typeof(TeamGroup), teamGroup)
-                                        + " isn't implemented for extension method IsPartOfTeamGroup.");
+                                        + "\" isn't implemented for extension method \"IsPartOfTeamGroup.\"");
             }
         }
 
@@ -53,7 +56,7 @@ namespace ZeroFootPrintSociety.CoreGame
         #region TeamGroup Enum Extensions
 
         public static bool IncludesTeam(this TeamGroup teamGroup, Team subjectTeam)
-            => subjectTeam.IsPartOfTeamGroup(teamGroup);
+            => subjectTeam.IsIncludedIn(teamGroup);
 
         #endregion
     }
