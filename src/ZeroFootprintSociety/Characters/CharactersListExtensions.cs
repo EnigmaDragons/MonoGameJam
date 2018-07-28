36c1dd58d954c4c701a2c62e0a70c1e8cf2f1f39
Félix Dion Robidoux <felix@fdrobidoux.com>
2018-07-28 18:56:13 -0400

    Added extensions to List<Character>. Add more if you want.

---
+++ src/ZeroFootprintSociety/Characters/CharactersListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroFootPrintSociety.Characters.Teams;
+
+namespace ZeroFootPrintSociety.Characters
+{
+    public static class CharactersListExtensions
+    {
+        public static IEnumerable<IGrouping<Team, Character>> GroupByTeam(this List<Character> listCharacters)
+            => listCharacters.GroupBy(x => x.Team, x => x);
+    }
+}
