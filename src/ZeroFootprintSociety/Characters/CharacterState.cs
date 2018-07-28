8a1ee3b646e76d6f745bb893ac7948164b332880
Félix Dion Robidoux <felix@fdrobidoux.com>
2018-07-28 18:55:48 -0400

    Make sure Health never goes under 0 and over `_stats.HP`

--- src/ZeroFootprintSociety/Characters/CharacterState.cs
+++ src/ZeroFootprintSociety/Characters/CharacterState.cs
@@ -1,10 +1,18 @@
-﻿namespace ZeroFootPrintSociety.Characters
+﻿using Microsoft.Xna.Framework;
+
+namespace ZeroFootPrintSociety.Characters
 {
     public class CharacterState
     {
         private readonly CharacterStats _stats;
+        private int _remainingHealth = 0;
 
-        public int RemainingHealth { get; set; }
+        public int RemainingHealth
+        {
+            get => _remainingHealth;
+            set => _remainingHealth = MathHelper.Clamp(value, 0, _stats.HP);
+        }
+
         public float PercentLeft => (float)RemainingHealth / _stats.HP;
 
         public CharacterState(CharacterStats stats)
