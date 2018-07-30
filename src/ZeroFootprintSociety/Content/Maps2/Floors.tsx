064c3961ad3d04d8c00e31a3ae9ade4d09eaab03
Febucci <40569257+febucci@users.noreply.github.com>
2018-07-30 23:56:47 +0200

    Added tags in the maps (only walls are set)

--- src/ZeroFootprintSociety/Content/Maps2/Floors.tsx
+++ src/ZeroFootprintSociety/Content/Maps2/Floors.tsx
@@ -1,4 +1,244 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Floors" tilewidth="24" tileheight="24" tilecount="110" columns="10">
  <image source="Floors.png" width="246" height="264"/>
+ <tile id="0">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="Blocked" type="bool" value="false"/>
+   <property name="Cover" value="None"/>
+  </properties>
+ </tile>
 </tileset>
