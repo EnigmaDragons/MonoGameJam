5a7a7152a7c0912e0b07484935921e0a6bcc32f1
Febucci <40569257+febucci@users.noreply.github.com>
2018-07-30 23:00:57 +0200

    Improved sprite sheets and modified the tile sets.

--- src/ZeroFootprintSociety/Content/Maps/Floors.tsx
+++ src/ZeroFootprintSociety/Content/Maps/Floors.tsx
@@ -1,9 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Floors" tilewidth="24" tileheight="24" tilecount="240" columns="12">
- <image source="Floors.png" width="288" height="480"/>
- <tile id="90">
-  <properties>
-   <property name="Blocking" type="bool" value="true"/>
-  </properties>
- </tile>
+<tileset name="Floors" tilewidth="24" tileheight="24" tilecount="110" columns="10">
+ <image source="Floors.png" width="246" height="264"/>
 </tileset>
--- src/ZeroFootprintSociety/Content/Maps/Objects.tsx
+++ src/ZeroFootprintSociety/Content/Maps/Objects.tsx
@@ -1,9 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Objects" tilewidth="24" tileheight="24" tilecount="792" columns="12">
- <image source="Objects.png" width="288" height="1600"/>
- <tile id="158">
-  <properties>
-   <property name="Blocking" type="bool" value="true"/>
-  </properties>
- </tile>
+<tileset name="Objects" tilewidth="24" tileheight="24" tilecount="833" columns="17">
+ <image source="Objects.png" width="408" height="1176"/>
 </tileset>
