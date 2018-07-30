7bbd593cb16310c6a5f1d961263089a4b121f767
SilasReinagel <silas.reinagel@gmail.com>
2018-07-29 21:16:32 -0700

    2nd Layer of PostFX. Added Circle Gradient PostFX. Closes #357. Closes #359

--- src/ZeroFootprintSociety/CoreGame/GameDrawMaster.cs
+++ src/ZeroFootprintSociety/CoreGame/GameDrawMaster.cs
@@ -16,7 +16,6 @@ namespace ZeroFootPrintSociety.CoreGame
         const int UnderChar2 = 3;
         const int OverChar1 = 4;
         const int OverChar2 = 5;
-        const int PostFx = 6;
 
         public void Draw(Transform2 parentTransform)
         {
@@ -37,7 +36,6 @@ namespace ZeroFootPrintSociety.CoreGame
                 x.Draw(OverChar1, parentTransform);
                 x.Draw(OverChar2, parentTransform);
             });
-
             GameWorld.Map.Tiles.ForEach(x => _fx.Draw(parentTransform, x));
         }
     }
--- src/ZeroFootprintSociety/Program.cs
+++ src/ZeroFootprintSociety/Program.cs
@@ -23,7 +23,7 @@ namespace ZeroFootPrintSociety
         [STAThread]
         static void Main()
         {
-            RunGame("Level1");
+            RunGame("SampleLevel");
         }
 
         private static SceneFactory CreateSceneFactory()
