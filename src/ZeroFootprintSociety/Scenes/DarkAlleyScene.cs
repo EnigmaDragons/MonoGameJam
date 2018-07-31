94b72e80544e73403a9db4a3df7eb4838bc69617
Febucci <40569257+febucci@users.noreply.github.com>
2018-07-31 16:53:28 +0200

    Added Dark alley scene

---
+++ src/ZeroFootprintSociety/Scenes/DarkAlleyScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.PhysicsEngine;
+using MonoDragons.Core.Scenes;
+using MonoTiled.Tiled.TmxLoading;
+using ZeroFootPrintSociety.Characters;
+using ZeroFootPrintSociety.CoreGame;
+using ZeroFootPrintSociety.Tiles;
+using Microsoft.Xna.Framework;
+using ZeroFootPrintSociety.Characters.Prefabs;
+
+namespace ZeroFootPrintSociety.Scenes
+{
+    public sealed class DarkAlleyScene : SimpleScene
+    {
+        private TacticsGame _game;
+
+        public override void Init()
+        {
+            GameWorld.Map = new GameMapFactory().CreateGameMap(new Tmx(CurrentGame.GraphicsDevice, "Maps2", "DarkAlley.tmx"), new Size2(100, 100));
+            GameWorld.Characters = new List<Character>
+            {
+                new MainChar().Initialized(GameWorld.Map[49, 97]),
+                //new Sidechick().Initialized(GameWorld.Map[50, 98]),
+            };
+
+            var startingCameraTile = new Point(49, 54);
+            _game = new TacticsGame(
+                new TurnBasedCombat(
+                    GameWorld.Map,
+                    GameWorld.Characters),
+                startingCameraTile);
+            _game.Init();
+            Add(_game);
+        }
+
+        public override void Dispose() { }
+    }
+}
