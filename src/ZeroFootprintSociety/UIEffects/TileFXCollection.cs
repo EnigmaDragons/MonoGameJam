3f14caac94ed146dcd269d4d6a021862e818f07d
Febucci <40569257+febucci@users.noreply.github.com>
2018-07-31 02:09:03 +0200

    PostFX tiles can be moved by half tile.

--- src/ZeroFootprintSociety/UIEffects/TileFXCollection.cs
+++ src/ZeroFootprintSociety/UIEffects/TileFXCollection.cs
@@ -21,46 +21,48 @@ namespace ZeroFootPrintSociety.UIEffects
             tile.PostFX.ForEach(x =>
             {
                 var fx = new FXString(x);
-                if (fx.Name.Equals("trianglegradient", StringComparison.InvariantCultureIgnoreCase))
-                    DrawTriangleGradient(parentTransform, tile, _colors[fx.Color]);
-                else if (fx.Name.Equals("circlegradient", StringComparison.InvariantCultureIgnoreCase))
-                    DrawCircleGradient(parentTransform, tile, _colors[fx.Color]);
-                else if (fx.Name.Equals("smallcirclegradient", StringComparison.InvariantCultureIgnoreCase))
-                    DrawSmallCircleGradient(parentTransform, tile, _colors[fx.Color]);
-                else if (fx.Name.Equals("vertneongradient", StringComparison.InvariantCultureIgnoreCase))
-                    DrawVertNeonGradient(parentTransform, tile, _colors[fx.Color]);
-                else
-                    Logger.WriteLine($"Unknown FX '{x}'");
+                string lower_name = fx.Name.ToLowerInvariant();
+
+                //The id is formed by "X_FXNAME_COLOR", where X is the offset
+                //We're searching the FX id, so we make a substring that starts from 2
+                switch (lower_name.Substring(2))
+                {
+                    case "trianglegradient": DrawTriangleGradient(parentTransform, tile, _colors[fx.Color], fx.Offset); break;
+                    case "circlegradient": DrawCircleGradient(parentTransform, tile, _colors[fx.Color], fx.Offset); break;
+                    case "smallcirclegradient": DrawSmallCircleGradient(parentTransform, tile, _colors[fx.Color], fx.Offset); break;
+                    case "vertneongradient": DrawVertNeonGradient(parentTransform, tile, _colors[fx.Color], fx.Offset); break;
+                    default: Logger.WriteLine($"Unknown FX '{x}'"); break;
+                }
             });
         }
 
-        private void DrawTriangleGradient(Transform2 parentTransform, GameTile tile, Color color)
+        private void DrawTriangleGradient(Transform2 parentTransform, GameTile tile, Color color, bool x_offset)
         {
-            var gradientOffset = new Vector2(-388, -740);
+            var gradientOffset = new Vector2(-388 + (x_offset ? 24 : 0), -740);
             var loc = (TileData.RenderSize.ToPoint() * tile.Position).ToVector2();
             var t = new Transform2(loc + gradientOffset, new Size2(800, 800));
             UI.Draw("Effects/TriangleGradient", parentTransform + t, color);
         }
 
-        private void DrawCircleGradient(Transform2 parentTransform, GameTile tile, Color color)
+        private void DrawCircleGradient(Transform2 parentTransform, GameTile tile, Color color, bool x_offset)
         {
-            var gradientOffset = new Vector2(-122, -128);
+            var gradientOffset = new Vector2(-122 + (x_offset ? 24 : 0), -128);
             var loc = (TileData.RenderSize.ToPoint() * tile.Position).ToVector2();
             var t = new Transform2(loc + gradientOffset, new Size2(290, 290));
             UI.Draw("Effects/CircleGradient", parentTransform + t, color);
         }
 
-        private void DrawSmallCircleGradient(Transform2 parentTransform, GameTile tile, Color color)
+        private void DrawSmallCircleGradient(Transform2 parentTransform, GameTile tile, Color color, bool x_offset)
         {
-            var gradientOffset = new Vector2(-46, -20);
+            var gradientOffset = new Vector2(-46 + (x_offset ? 24 : 0), -20);
             var loc = (TileData.RenderSize.ToPoint() * tile.Position).ToVector2();
             var t = new Transform2(loc + gradientOffset, new Size2(140, 140));
             UI.Draw("Effects/CircleGradient", parentTransform + t, color);
         }
 
-        private void DrawVertNeonGradient(Transform2 parentTransform, GameTile tile, Color color)
+        private void DrawVertNeonGradient(Transform2 parentTransform, GameTile tile, Color color, bool x_offset)
         {
-            var gradientOffset = new Vector2(0, -48);
+            var gradientOffset = new Vector2(0 + (x_offset ? 24 : 0), -48);
             var loc = (TileData.RenderSize.ToPoint() * tile.Position).ToVector2();
             var t = new Transform2(loc + gradientOffset, new Size2(96, 192));
             UI.Draw("Effects/VertNeonGradient", parentTransform + t, color);
