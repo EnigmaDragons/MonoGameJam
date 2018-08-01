cd0e60192dfc3ffcd8bb841f303d561d22d75f45
SilasReinagel <silas.reinagel@gmail.com>
2018-08-01 12:19:56 -0700

    Footprints update

---
+++ src/ZeroFootprintSociety/GUI/UiEffects/FadingVisual.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonoDragons.Core.Animations;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.PhysicsEngine;
+using MonoDragons.Core.UserInterface;
+
+namespace ZeroFootPrintSociety.GUI
+{
+    public class FadingVisual : IAnimation, IVisualAutomaton
+    {
+        private readonly IVisual _visual;
+        private readonly Action<Color> _setColor;
+
+        public int FromAlpha { get; set; } = 255;
+        public int ToAlpha { get; set; } = 0;
+        public TimeSpan Duration { get; set; } = TimeSpan.FromSeconds(1);
+        public Color SourceColor { get; set; }
+        
+        private TimeSpan _elapsed;
+        private bool _started;
+        private int _currentAlpha;
+        private Action _onFinished;
+
+        public FadingVisual(IVisual visual, Action<Color> setColor)
+        {
+            _visual = visual;
+            _setColor = setColor;
+        }
+        
+        public void Start(Action onFinished)
+        {
+            if (_started) return;
+
+            _onFinished = onFinished;
+            _setColor(SourceColor.WithAlpha(FromAlpha));
+            _started = true;
+        }
+
+        public void Update(TimeSpan delta)
+        {
+            IfRunning(() =>
+            {
+                _elapsed += delta;
+                _currentAlpha = _elapsed >= Duration
+                    ? ToAlpha
+                    : (int)(MathHelper.Lerp(FromAlpha, ToAlpha, (float)(_elapsed.TotalMilliseconds / Duration.TotalMilliseconds)));
+                _setColor(SourceColor.WithAlpha(_currentAlpha));
+                if (_elapsed >= Duration)
+                    _onFinished();
+            });
+        }
+
+        public void Draw(Transform2 parentTransform)
+        {
+            IfRunning(() => _visual.Draw(parentTransform));
+        }
+
+        private void IfRunning(Action action)
+        {
+            if (_started && _elapsed <= Duration)
+                action();
+        }
+    }
+}
---
+++ src/ZeroFootprintSociety/Tiles/FadingSpriteAnimation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MonoDragons.Core.Animations;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.PhysicsEngine;
+
+namespace ZeroFootPrintSociety.Tiles
+{
+    public class FadingSpriteAnimation : IVisualAutomaton, IAnimation
+    {
+        private readonly SpriteAnimation _anim;
+        private readonly TimeSpan _duration;
+        private TimerTask _task;
+        private bool _running;
+
+        public FadingSpriteAnimation(SpriteAnimation anim, TimeSpan duration)
+        {
+            _anim = anim;
+            _duration = duration;
+        }
+      
+        public void Update(TimeSpan delta)
+        {
+            if (_running)
+            {
+                _task.Update(delta);
+                _anim.Update(delta);
+            }
+        }
+
+        public void Draw(Transform2 parentTransform)
+        {
+            if (_running)
+                _anim.Draw(parentTransform);
+        }
+
+        public void Start(Action onFinished)
+        {
+            _task = new TimerTask(() =>
+            {
+                _running = false;
+                onFinished();
+                _anim.Reset();
+            }, _duration.TotalMilliseconds, false);
+            _running = true;
+        }
+    }
+}
