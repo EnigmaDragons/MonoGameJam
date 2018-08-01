7a689ca055d72a4ed73ea601437aeeca52f9a378
SilasReinagel <silas.reinagel@gmail.com>
2018-08-01 09:45:29 -0700

    Updated Credits scene. Closes #573. Closes #564

--- src/ZeroFootprintSociety/Credits/CreditsSegments.cs
+++ src/ZeroFootprintSociety/Credits/CreditsSegments.cs
@@ -1,87 +1,123 @@
 ﻿
+using Microsoft.Xna.Framework;
+
 namespace ZeroFootPrintSociety.Credits
 {
-    public sealed class EnigmaDragonsCredit : BasicJamCreditSegment
+    public static class CreditColors
     {
-        public override string Role => "Enigma Dragons";
-        public override string Name => "";
+        
+        public static readonly Color Backend = Color.FromNonPremultiplied(120, 80, 255, 255);
+        public static readonly Color Project = Color.FromNonPremultiplied(50, 255, 50, 255);
+        public static readonly Color Frontend = Color.FromNonPremultiplied(255, 80, 80, 255);
+        public static readonly Color Gameplay = Color.FromNonPremultiplied(50, 160, 255, 255);
     }
-
+    
+    public static class CreditNames
+    {
+        public const string Noah = "Noah Reinagel";
+        public const string Silas = "Silas Reinagel";
+        public const string Felix = "Felix Dion-Robidoux";
+        public const string Federico = "Federico Bellucci";
+        public const string Tim = "Tim Reinagel";
+        public const string Herbert = "Herbert Losloso";
+        public const string Gordy = "Gordy Keene";
+    }
+    
     public sealed class ProjectManagerCredit : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Project;
         public override string Role => "Project Manager";
-        public override string Name => "Silas Reinagel";
+        public override string Name => CreditNames.Silas;
     }
 
-    public sealed class GameDesignerCredit : BasicJamCreditSegment
+    public sealed class LeadGameDesignerCredit : BasicJamCreditSegment
     {
-        public override string Role => "Game Designer";
-        public override string Name => "Silas Reinagel";
+        public override Color RoleColor => CreditColors.Gameplay;
+        public override string Role => "Lead Game Designer";
+        public override string Name => CreditNames.Silas;
     }
 
     public sealed class UiDesignerCredit : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Frontend;
         public override string Role => "UI Designer";
-        public override string Name => "Silas Reinagel";
+        public override string Name => CreditNames.Silas;
     }
-
+    
+    public sealed class AudioCredit : BasicJamCreditSegment
+    {
+        public override Color RoleColor => CreditColors.Frontend;
+        public override string Role => "Sound / Music";
+        public override string Name => CreditNames.Silas;
+    }
+    
     public sealed class LeadArtistCredit : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Frontend;
         public override string Role => "Lead Artist";
-        public override string Name => "Federico Bellucci";
+        public override string Name => CreditNames.Federico;
     }
 
     public sealed class EnvironmentArtCredit : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Frontend;
         public override string Role => "Environment Art";
-        public override string Name => "Federico Bellucci";
+        public override string Name => CreditNames.Federico;
     }
 
-    public sealed class LevelDesignerCredit : BasicJamCreditSegment
+    public sealed class LevelDesigner1Credit : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Gameplay;
         public override string Role => "Level Designer";
-        public override string Name => "Federico Bellucci";
+        public override string Name => CreditNames.Federico;
     }
 
-    public sealed class ComposerCredit : BasicJamCreditSegment
+    public sealed class LevelDesigner2Credit : BasicJamCreditSegment
     {
-        public override string Role => "Music Composer";
-        public override string Name => "Joseph Gomes";
+        public override Color RoleColor => CreditColors.Gameplay;
+        public override string Role => "Level Designer";
+        public override string Name => CreditNames.Noah;
     }
-
-    public sealed class WriterCredit : BasicJamCreditSegment
-    {
-        public override string Role => "Story & Dialogue Writer";
-        public override string Name => "TBD";
-    }
-
+    
     public sealed class LeadProgrammerCredit : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Backend;
         public override string Role => "Lead Programmer";
-        public override string Name => "Noah Reinagel";
+        public override string Name => CreditNames.Noah;
     }
 
+    public sealed class GameplayProgrammerCredit : BasicJamCreditSegment
+    {
+        public override Color RoleColor => CreditColors.Backend;
+        public override string Role => "Gameplay / Engine Programmer";
+        public override string Name => CreditNames.Noah;
+    }
+    
     public sealed class ProgrammerCredit : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Backend;
         public override string Role => "Programmer";
-        public override string Name => "Felix Robidoux";
+        public override string Name => CreditNames.Felix;
     }
 
     public sealed class AlgorithmOptimizer : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Backend;
         public override string Role => "Algorithm Optimizer";
-        public override string Name => "Tim Reinagel";
+        public override string Name => CreditNames.Tim;
     }
 
     public sealed class CharacterArtistCredit : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Frontend;
         public override string Role => "Character Artist";
-        public override string Name => "Herbert Losloso";
+        public override string Name => CreditNames.Herbert;
     }
 
     public sealed class Tester1Credit : BasicJamCreditSegment
     {
+        public override Color RoleColor => CreditColors.Project;
         public override string Role => "Game Tester";
-        public override string Name => "Gordy Keene";
+        public override string Name => CreditNames.Gordy;
     }
 }
