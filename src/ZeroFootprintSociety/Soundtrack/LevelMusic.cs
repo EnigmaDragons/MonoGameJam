6a3683a09b62aa8bda146de0cd798c1d8ec0e991
SilasReinagel <silas.reinagel@gmail.com>
2018-08-01 10:21:04 -0700

    Tuning music experience

--- src/ZeroFootprintSociety/Soundtrack/LevelMusic.cs
+++ src/ZeroFootprintSociety/Soundtrack/LevelMusic.cs
@@ -7,25 +7,26 @@ namespace ZeroFootPrintSociety.Soundtrack
     public sealed class LevelMusic
     {
         private readonly DictionaryWithDefault<MusicType, string> _songs;
+        private readonly DictionaryWithDefault<MusicType, float> _volumes;
         private readonly List<Sound> _sounds = new List<Sound>();
 
-        public LevelMusic(string ambientSongName)
-        {
-            _songs = new DictionaryWithDefault<MusicType, string>(ambientSongName);
-        }
-
-        public LevelMusic(string ambientSongName, string actionSongName, string bossSongName)
+        public LevelMusic(string ambientSongName, string actionSongName, string bossSongName, float actionMusicVolume = 0.6f)
         {
             _songs = new DictionaryWithDefault<MusicType, string>(ambientSongName)
             {
                 {MusicType.Action, actionSongName},
                 {MusicType.Boss, bossSongName}
             };
+            _volumes = new DictionaryWithDefault<MusicType, float>(0.6f)
+            {
+                {MusicType.Action, actionMusicVolume},
+                {MusicType.Boss, actionMusicVolume}
+            };
         }
 
         public void Play(MusicType musicType)
         {
-            var music = Sound.Music(_songs[musicType]);
+            var music = Sound.Music(_songs[musicType], _volumes[musicType]);
             music.Play();
             _sounds.Add(music);
         }
