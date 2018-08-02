c8842eb5f7ea91896a2d589dbe367208d119fc35
noah reinagel <noah.r9@gmail.com>
2018-08-01 20:50:29 -0700

    added villains face

--- src/ZeroFootprintSociety/Characters/Prefabs/CorpSec3.cs
+++ src/ZeroFootprintSociety/Characters/Prefabs/CorpSec3.cs
@@ -22,7 +22,7 @@ namespace ZeroFootPrintSociety.Characters.Prefabs
             },
             new CharacterGear(WeaponLists.RandomPrimary(), WeaponLists.RandomSecondary()),
             Team.Enemy,
-            "Characters/placeholder-soldier-face.png",
+            "Characters/CorporationSecurity3/Corporate_Villain_face.png",
             Bust)
         {
             State.MustKill = true;
